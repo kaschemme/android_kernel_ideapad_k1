870e09ef64e45b68f38a42490a978d33d733e75e
Christian Speckner <cnspeckn@googlemail.com>
2013-05-02 22:03:29 +0200

    Replace whole wifi subsys with the current google kernel -> compiles and works.

--- drivers/net/wireless/bcm4329/dhd.h
+++ drivers/net/wireless/bcm4329/dhd.h
@@ -48,10 +48,6 @@
 #include <asm/uaccess.h>
 #include <asm/unaligned.h>
 
-#ifdef CONFIG_HAS_WAKELOCK
-#include <linux/wakelock.h>
-#endif
-
 /* The kernel threading is sdio-specific */
 #else /* LINUX */
 #define ENOMEM		1
@@ -173,9 +169,6 @@ typedef struct dhd_pub {
 	wl_country_t dhd_cspec;		/* Current Locale info */
 	char eventmask[WL_EVENTING_MASK_LEN];
 
-#ifdef CONFIG_HAS_WAKELOCK
-	struct wake_lock 	wow_wakelock;
-#endif
 } dhd_pub_t;
 
 #if (LINUX_VERSION_CODE >= KERNEL_VERSION(2, 6, 27)) && defined(CONFIG_PM_SLEEP)
@@ -185,7 +178,7 @@ typedef struct dhd_pub {
 			int retry = 0; \
 			smp_mb(); \
 			while (dhd_mmc_suspend && retry++ != b) { \
-				wait_event_interruptible_timeout(a, FALSE, 3 * HZ); \
+				wait_event_interruptible_timeout(a, FALSE, HZ/100); \
 			} \
 		} 	while (0)
 	#define DHD_PM_RESUME_WAIT(a) 		_DHD_PM_RESUME_WAIT(a, 200)
