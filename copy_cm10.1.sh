7949753b917044146d3e2b2a59b782a9421ae59c
kaschemme <holgerdaehn@gmail.com>
2013-08-14 21:18:50 +0200

    working CM10.1 kernel

---
+++ copy_cm10.1.sh
@@ -0,0 +1,5 @@
+cp crypto/md4.ko ~/android/system2/device/lenovo/k1/prebuilt/md4.ko
+cp drivers/net/tun.ko ~/android/system2/device/lenovo/k1/prebuilt/tun.ko
+cp drivers/scsi/scsi_wait_scan.ko ~/android/system2/device/lenovo/k1/prebuilt/scsi_wait_scan.ko
+cp fs/cifs/cifs.ko ~/android/system2/device/lenovo/k1/prebuilt/cifs.ko
+cp arch/arm/boot/zImage ~/android/system2/device/lenovo/k1/kernel
